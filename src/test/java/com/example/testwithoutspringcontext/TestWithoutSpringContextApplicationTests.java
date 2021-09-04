4e2e1b0f02245aa105bfeb7eb8bf4368d672d099
Gyeongjun <lastpuzly@gmail.com>
2021-09-04 17:08:32 +0900

    chore: init project

---
+++ src/test/java/com/example/testwithoutspringcontext/TestWithoutSpringContextApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testwithoutspringcontext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestWithoutSpringContextApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
